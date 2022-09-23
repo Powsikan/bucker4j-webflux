0df1517fdd970a7b5bc939091c19326d445ef572
Ragulan Gopalapillai <ragulan28@gmail.com>
2022-09-23 16:02:33 +0530

    init

---
+++ src/main/java/com/example/backet4j/Backet4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.backet4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backet4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backet4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backet4j/Backet4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backet4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backet4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
