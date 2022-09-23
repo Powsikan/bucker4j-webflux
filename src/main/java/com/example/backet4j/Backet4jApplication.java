cd7864a3b447b573a94786f9cc552e0e43b5fb43
Ragulan Gopalapillai <ragulan28@gmail.com>
2022-09-23 16:20:14 +0530

    web

--- src/main/java/com/example/backet4j/Backet4jApplication.java
+++ src/main/java/com/example/backet4j/Backet4jApplication.java
@@ -2,8 +2,10 @@ package com.example.backet4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Backet4jApplication {
 
     public static void main(String[] args) {
