3f94fb8c95bea82a32dcfd14e4bc52c89551c0c8
Ragulan Gopalapillai <ragulan28@gmail.com>
2022-09-23 16:40:59 +0530

    web-flex

--- src/main/java/com/example/backet4j/TestController.java
+++ src/main/java/com/example/backet4j/TestController.java
@@ -3,6 +3,7 @@ package com.example.backet4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RequestMapping("test")
 @RestController
@@ -11,5 +12,9 @@ public class TestController {
     @GetMapping("testing")
     public String test(){
         return "test";
+    }
+    @GetMapping("testing-flex")
+    public Mono<String> testFlex(){
+        return Mono.just("testFlex");
     }
 }
